a5e0411f45c91726294a0beab5df5165aa7d85be
zhaoyongtao <285929232@qq.com>
2018-06-26 18:15:11 +0800

    第4步: 创建一个无限滚动ListView

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,19 @@ class MyApp extends StatelessWidget {
 //      theme: new ThemeData(
 //        primarySwatch: Colors.blue,
 //      ),
-      home: new Scaffold(
-        //Scaffold 是 Material library 中提供的一个widget, 它提供了默认的导航栏、标题和包含主屏幕widget树的body属性。widget树可以很复杂
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter_title'),
-        ),
-        body: new Center(
-          //本示例中的body的widget树中包含了一个Center widget, Center widget又包含一个 Text 子widget。 Center widget可以将其子widget树对其到屏幕中心
-//          child: new Text('hello world'),
-//          child: new Text(wordPair.asPascalCase),
-          child: new RandomWords(),
-        ),
-      ),
+//      home: new Scaffold(
+//        //Scaffold 是 Material library 中提供的一个widget, 它提供了默认的导航栏、标题和包含主屏幕widget树的body属性。widget树可以很复杂
+//        appBar: new AppBar(
+//          title: new Text('Welcome to Flutter_title'),
+//        ),
+//        body: new Center(
+//          //本示例中的body的widget树中包含了一个Center widget, Center widget又包含一个 Text 子widget。 Center widget可以将其子widget树对其到屏幕中心
+////          child: new Text('hello world'),
+////          child: new Text(wordPair.asPascalCase),
+//          child: new RandomWords(),
+//        ),
+//      ),
+      home: new RandomWords(),
     );
   }
 }
@@ -41,9 +42,53 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  //_suggestions列表以保存建议的单词对。 该变量以下划线（_）开头，在Dart语言中使用下划线前缀标识符，会强制其变成私有的
+  final _suggestions = <WordPair>[];
+
+  //biggerFont变量来增大字体大小
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+//    final wordPair = new WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Startup Name ListView')),
+      body: _buildSuggestions(),
+    );
+  }
+
+  //法构建显示建议单词对的ListView
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+        // 在偶数行，该函数会为单词对添加一个ListTile row.
+        // 在奇数行，该行书湖添加一个分割线widget，来分隔相邻的词对。
+        // 注意，在小屏幕上，分割线看起来可能比较吃力。
+        itemBuilder: (context, i) {
+          // 在每一列之前，添加一个1像素高的分隔线widget
+          if (i.isOdd) {
+            return new Divider(color: Colors.red);
+          }
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+          final index = i ~/ 2;
+          // 如果是建议列表中最后一个单词对
+          if (index >= _suggestions.length) {
+            // ...接着再生成10个单词对，然后添加到建议列表
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
